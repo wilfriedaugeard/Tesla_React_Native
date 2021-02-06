4ff526bd784f70d30b6670809e1c36fe9600ac38
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2021-02-06 14:28:26 +0100

    Ajout des boutons

--- src/components/CarItem/index.js
+++ src/components/CarItem/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 import styles from './styles'
+import StyledButton from '../StyledButton'
+
 
 const CarItem = (props) => {
     return (
@@ -13,6 +15,23 @@ const CarItem = (props) => {
                 <Text style={styles.title}>Model S</Text>
                 <Text style={styles.subtitle}>Starting at 69 420€</Text>
             </View>
+
+
+            <StyledButton
+                type="primary"
+                content={"Custom order"}
+                onPress={() => {
+                    console.warn("custom pressed")
+                }}
+            />
+            <StyledButton
+                type="secondary"
+                content={"Existing Inventory"}
+                onPress={() => {
+                    console.warn("Existing Inventory")
+                }}
+            />
+
         </View>
     )
 }
