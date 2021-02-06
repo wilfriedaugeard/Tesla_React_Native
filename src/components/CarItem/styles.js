afe599eec674eda5b7378124f603c87fb9eb582b
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2021-02-06 15:11:09 +0100

    Liste des voitures + headers

--- src/components/CarItem/styles.js
+++ src/components/CarItem/styles.js
@@ -1,13 +1,13 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
     carContainer: {
 		width: '100%',
-		height: '100%',
+		height: Dimensions.get('window').height,
 	},
 
 	titles: {
-		marginTop: '25%',
+		marginTop: '30%',
 		width: '100%',
 		alignItems: 'center'
 	},
